4dae4bc080949ea9eaf6cc344caa562cfd54f868
Adarsh <adarshsidhu83@gmail.com>
2025-02-17 13:03:05 -0500

    no changes

--- client/src/components/Users.tsx
+++ client/src/components/Users.tsx
@@ -26,35 +26,35 @@ const UserList: React.FC<UserListProps> = ({ users }) => {
     // })
 
     return (
-        <>
-            <h2 className="pb-5">
-                Check out all your friends!
-            </h2>
-            {users && users.map((user) => (
-                <div className="row align-center mb-5" key={user.id}>
-                    <div className="col-md-6">
-                        <h3>{user.id}. {user.username}</h3>
-                    </div>
-                    <div className="col-md-6">
-                        <h4><a href={`mailto:${user.email}`}>{user.email}</a></h4>
-                    </div>
-                </div>
-            ))}
-        </>
         // <>
-        //     <section className='container'>
-        //         <section>
-        //         <article className='allUsers'>
-
-        //         </article>
-        //         </section>
-        //         <section className='mainbodyforchatroom'>
-        //         <header></header>
-        //         <main></main>
-        //         <footer></footer>
-        //         </section>
-        //     </section>
+        //     <h2 className="pb-5">
+        //         Check out all your friends!
+        //     </h2>
+        //     {users && users.map((user) => (
+        //         <div className="row align-center mb-5" key={user.id}>
+        //             <div className="col-md-6">
+        //                 <h3>{user.id}. {user.username}</h3>
+        //             </div>
+        //             <div className="col-md-6">
+        //                 <h4><a href={`mailto:${user.email}`}>{user.email}</a></h4>
+        //             </div>
+        //         </div>
+        //     ))}
         // </>
+        <>
+            <section className='container'>
+                <section>
+                <article className='allUsers'>
+
+                </article>
+                </section>
+                <section className='mainbodyforchatroom'>
+                <header></header>
+                <main></main>
+                <footer></footer>
+                </section>
+            </section>
+        </>
     );
 };
 
